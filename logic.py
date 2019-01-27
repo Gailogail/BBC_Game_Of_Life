11cba5c14ed830cf159a281758c19b5363911228
TomEaterbrook <tomeasterbrook98@gmail.com>
2019-01-27 15:51:56 +0000

    Comments added to logic to improve readability

--- logic.py
+++ logic.py
@@ -11,58 +11,68 @@ import random
 def generate_grid(columns, rows):
     return [[random.randint(0,1)for x in range(columns)] for y in range(rows)]
 
+#evolves the grid by applying a Game of Life Principles
 def evolve_grid(grid):
    for column in range(len(grid)):
        for row in range(len(grid[column])):
           neighbours = check_neighbours(grid,column,row)
           if grid[column][row] == 1:
-               if neighbours<2 or neighbours>3:
+              # Over and under population
+              if neighbours<2 or neighbours>3:
                    grid[column][row] = 0
           else:
+              # Creation of life
               if neighbours == 3:
                   grid[column][row]=1
+   # Survival is default position if none of the above conditions are met
 
 
-
-
+# Checks how many neighbours of a specified cell are currently alive
 def check_neighbours(grid,start_column,start_row):
     count = 0
+    #N
     try:
         if grid[start_column + 1][start_row] == 1:
             count += 1
     except:
         pass
-
+    #S
     try:
         if grid[start_column - 1][start_row] == 1:
             count += 1
     except:
         pass
+    #E
     try:
         if grid[start_column][start_row+1] == 1:
             count += 1
     except:
         pass
+    #W
     try:
         if grid[start_column][start_row-1] == 1:
             count += 1
     except:
         pass
+    #NE
     try:
         if grid[start_column+1][start_row + 1] == 1:
             count += 1
     except:
         pass
+    #SW
     try:
         if grid[start_column-1][start_row - 1] == 1:
             count += 1
     except:
         pass
+    #NW
     try:
         if grid[start_column+1][start_row - 1] == 1:
             count += 1
     except:
         pass
+    #NE
     try:
         if grid[start_column-1][start_row + 1] == 1:
             count += 1
@@ -70,6 +80,3 @@ def check_neighbours(grid,start_column,start_row):
         pass
 
     return count
-
-
-print(generate_grid(2, 3))
