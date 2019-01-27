f652cc8d8d69d24c29b79f302c782646e35dafc5
TomEaterbrook <tomeasterbrook98@gmail.com>
2019-01-27 15:20:42 +0000

    Game of Life logic implemented and displayed on-screen at a rate of one evolution per second

--- Main.py
+++ Main.py
@@ -16,14 +16,14 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 
 # This sets the WIDTH and HEIGHT of each cell
-CELL_W = 5
-CELL_H = 5
+CELL_W = 20
+CELL_H = 20
 
 # This sets the gap between each cell
-GAP = 5
+GAP = 20
 
 # Generate  grid
-grid = l.generate_grid(100,100)
+grid = l.generate_grid(25,25)
 
 # start pygame
 pygame.init()
@@ -61,10 +61,11 @@ while not closed:
                             CELL_H])
 
     # Limit to 60 frames per second
-    clock.tick(60)
+    clock.tick(1)
 
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
+    l.evolve_grid(grid)
 
     # Be IDLE friendly. If you forget this line, the program will 'hang'
     # on exit.
--- logic.py
+++ logic.py
@@ -12,9 +12,64 @@ def generate_grid(columns, rows):
     return [[random.randint(0,1)for x in range(columns)] for y in range(rows)]
 
 def evolve_grid(grid):
-    pass
-def check_neighbours(grid,start_x,start_y):
-    pass
+   for column in range(len(grid)):
+       for row in range(len(grid[column])):
+          neighbours = check_neighbours(grid,column,row)
+          if grid[column][row] == 1:
+               if neighbours<2 or neighbours>3:
+                   grid[column][row] = 0
+          else:
+              if neighbours == 3:
+                  grid[column][row]=1
+
+
+
+
+def check_neighbours(grid,start_column,start_row):
+    count = 0
+    try:
+        if grid[start_column + 1][start_row] == 1:
+            count += 1
+    except:
+        pass
+
+    try:
+        if grid[start_column - 1][start_row] == 1:
+            count += 1
+    except:
+        pass
+    try:
+        if grid[start_column][start_row+1] == 1:
+            count += 1
+    except:
+        pass
+    try:
+        if grid[start_column][start_row-1] == 1:
+            count += 1
+    except:
+        pass
+    try:
+        if grid[start_column+1][start_row + 1] == 1:
+            count += 1
+    except:
+        pass
+    try:
+        if grid[start_column-1][start_row - 1] == 1:
+            count += 1
+    except:
+        pass
+    try:
+        if grid[start_column+1][start_row - 1] == 1:
+            count += 1
+    except:
+        pass
+    try:
+        if grid[start_column-1][start_row + 1] == 1:
+            count += 1
+    except:
+        pass
+
+    return count
 
 
 print(generate_grid(2, 3))
